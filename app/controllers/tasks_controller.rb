b1b99ad6c2cc3c6d7ea3e1b22449ffba21e7a80a
weblabs7 <weblabs@live.com>
2017-07-03 12:59:21 +0000

    added simple form gem along with bootstrap infromation.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,43 @@
 class TasksController < ApplicationController
+    before_action :find_task, only:[:show, :edit, :update, :destroy]
     
     def index
     end
     
-    def create
-        @task = Task.new
+    def show
     end
     
+    def create
+        @task = Task.new(tasks_params)
+        
+        if @task.save
+            redirect_to @task
+        else
+            render 'New'
+        end
+    end
+    
+    def new
+        @task = Task.new
+        
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    private
+    
+    def tasks_params
+        params_require(:task).permit(:title, :description, :company, :url)
+    end
+    
+    def find_task
+        @task = Task.find(params[:id])
+    end
 end
